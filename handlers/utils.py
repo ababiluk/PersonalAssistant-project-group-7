9299c2e7632cd6c1e9c26da63da2a95a8c0a5431
Victoria Lytvyn <victoria.lytvyn27@gmail.com>
2026-05-27 23:36:00 +0100

    Extract command validation from main

--- handlers/utils.py
+++ handlers/utils.py
@@ -1,5 +1,8 @@
 import pickle
 from models import AddressBook
+import difflib
+from commands import commands
+from handlers.display import show_help, _print
 
 
 def parse_input(user_input):
@@ -19,3 +22,47 @@ def load_data(filename="addressbook.pkl"):
             return pickle.load(f)
     except FileNotFoundError:
         return AddressBook()
+
+def suggest_command(user_input): #guess command of user input
+    matches = difflib.get_close_matches(user_input, commands, n=1, cutoff=0.5)
+    
+    if matches:
+        return f"Invalid command. Maybe you meant '{matches}'? Type 'help' for all commands."
+    else:
+        return "Invalid command. Type 'help' for the command list."
+    
+def get_validated_command(user_command, available_commands, args, book):
+    if user_command in available_commands:
+        return user_command
+    matches = difflib.get_close_matches(
+        user_command, available_commands, n=5, cutoff=0.6
+    )
+    if len(matches) == 1:
+        suggested_command = matches[0]
+        confirm = input(f"Did you mean '{suggested_command}' (y/n)").strip().lower()
+        if confirm in ["y", "yes"]:
+            return suggested_command
+        _print(show_help(args, book))
+    elif len(matches) > 1:
+        _print("\nPossible commands:")
+
+        for i, match in enumerate(matches, 1):
+            _print(f"{i}. {match}")
+
+        choice = input("Choose command number (or type 'exit' to cancel): ").strip().lower()
+        if choice in ["exit", "close", "cancel"]:
+            return None
+        
+        if choice in matches:
+            return choice
+
+        try:
+            return matches[int(choice) - 1]
+        except (ValueError, IndexError):
+            _print("[red]Invalid selection. Please enter a number from the list.[/red]")
+    else:
+        _print(
+            "[yellow]Invalid command.[/yellow] "
+            "Type [green]help[/green] for the command list."
+        )
+    return None
--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-from handlers.display import (
-    show_help,
-    _print
-)
+from handlers.display import _print
 from commands import commands
-from handlers import (
-    parse_input,
-    save_data, 
-    load_data
-)
+from handlers import parse_input, save_data, load_data, get_validated_command
+
 
 def main():
     book = load_data()  # getting data from file on load
-    _print("[bold cyan]Welcome to the assistant bot![/bold cyan] Type [green]help[/green] for the command list.")
-
+    _print(
+        "[bold cyan]Welcome to the assistant bot![/bold cyan] Type [green]help[/green] for the command list."
+    )
+    available_commands = list(commands.keys())
     while True:
         try:
             user_input = input("Enter a command: ").strip()
@@ -21,24 +17,25 @@ def main():
 
             command, *args = parse_input(user_input)
             if command in ["close", "exit"]:
-                    save_data(book)
-                    print("Good bye!")
-                    break
-            handler = commands.get(command)
-                
-            if handler:
+                save_data(book)
+                print("Good bye!")
+                break
+
+            target_command = get_validated_command(
+                command, available_commands, args, book
+            )
+
+            if target_command:
+                handler = commands.get(target_command)
                 result = handler(args, book)
 
                 if result:
                     _print(result)
-            else:
-                _print(
-                    "[yellow]Invalid command.[/yellow] "
-                    "Type [green]help[/green] for the command list."
-                )
+
         except KeyboardInterrupt:
             print("\nCommand has been interrupted. Try again.")
-            continue 
-        
+            continue
+
+
 if __name__ == "__main__":
     main()
